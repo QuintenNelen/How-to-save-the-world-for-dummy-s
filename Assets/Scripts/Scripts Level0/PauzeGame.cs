f93bb059f3b73150a551c62d29af92ddbe75f13f
Fleur Van Buiijten <fleurvanbuijten@gmail.com>
2023-01-07 00:03:14 +0100

    Add pauze UI and script

---
+++ Assets/Scripts/Scripts Level0/PauzeGame.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauzeGame : MonoBehaviour
+{
+    [SerializeField] GameObject gamePauze;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P)) // for now this will be key p in unity 
+        {
+            gamePauze.SetActive(true); //if esc key is pressed show this UI
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
+    }
+
+    public void ContinueGame()
+    {
+        gamePauze.SetActive(false); //Turns UI off again and you can continue playing the game
+        //Debug.Log("continue");
+    }
+
+    public void QuitGameInPauze()
+    {
+        Application.Quit(); // quits the game
+        Debug.Log("Quit");
+    }
+}
