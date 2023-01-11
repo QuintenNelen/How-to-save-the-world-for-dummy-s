f02c0a37b0a0f990d53680f03880bbd4a7678e67
BerrieV1 <114003039+BerrieV1@users.noreply.github.com>
2023-01-11 18:54:18 +0100

    Add walking sound

---
+++ Assets/Scripts/FootSteps.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource footsteps;
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            footsteps.enabled = true;
+        }
+        else
+        {
+            footsteps.enabled = false;
+        }
+    }
+}
