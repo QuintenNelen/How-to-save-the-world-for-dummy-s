95e1196d5e4328ea2ec316c9b2b54f26b7e1052d
QuintenNelen <quinten.nelen@live.be>
2023-01-12 16:09:30 +0100

    Walking asset

---
+++ Assets/Scripts/walkingMusic.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walkingMusic : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource footsteps;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Z) || Input.GetKeyUp(KeyCode.Q) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D)  )
+        {
+            footsteps.enabled = true;
+        }
+        else
+        {
+            footsteps.enabled = false;
+        }
+    }
+}
