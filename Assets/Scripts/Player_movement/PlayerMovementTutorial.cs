e95532626ae1de58e89afe4cbdbbc4f3857c47e6
BerrieV1 <114003039+BerrieV1@users.noreply.github.com>
2023-01-14 11:01:10 +0100

    commit mergo into bernd

--- Assets/Scripts/Player_movement/PlayerMovementTutorial.cs
+++ Assets/Scripts/Player_movement/PlayerMovementTutorial.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+//movement player
+
 public class PlayerMovementTutorial : MonoBehaviour
 {
     [Header("Movement")]
---
+++ Assets/Scripts/Stepping.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//stappen + geluid toevoegen van stappen
+public class Stepping : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource stepping;
+    // Start is called before the first frame update
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            stepping.enabled = true;
+        } 
+        else
+        {
+
+            stepping.enabled = false;
+
+        }
+        
+    }
+}
