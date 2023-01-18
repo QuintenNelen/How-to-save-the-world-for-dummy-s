0dd77cf770f0b1bdd65635bd758960948529a261
QuintenNelen <quinten.nelen@live.be>
2023-01-18 19:49:03 +0100

    fix6

--- Assets/Scripts/collect.cs
+++ Assets/Scripts/collect.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 //items op pikken en vernietig  
 public class collect : MonoBehaviour
 {
-    public AudioSource audioSource;
+    public AudioSource audioSource2;
     //script dat zorgt dat de pick-up wordt verwijderd bij een trigger
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            audioSource.Play(); 
+            audioSource2.Play();
             Destroy(gameObject);
             nextLevel.pickedUp = true; //zet de status van een variable op "true" zodat we naar het volgende level kunnen. als deze variable "false" is, kan er niet naar het volgende level gegaan worden
         }
